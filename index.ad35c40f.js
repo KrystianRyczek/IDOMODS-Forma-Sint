cb3474646bc941dbfa08cfaeb2201c188ca0e0fc
KrystianRyczek <153994194+KrystianRyczek@users.noreply.github.com>
2025-05-29 16:52:43 +0000

    Deploying to gh-pages from @ KrystianRyczek/IDOMODS-Forma-Sint@66e7d663967f2ebcf0dafd68ed31ff85298d71b2 🚀

---
+++ index.ad35c40f.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{slidesPerView:4,spaceBetween:24,loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-next-btn",prevEl:".swiper-prev-btn"}});
+//# sourceMappingURL=index.ad35c40f.js.map
